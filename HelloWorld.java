f9a883f4c2d7111121d9a26cc07bb4a99572a26c
omar <omamourad1994@gmail.com>
2021-07-27 00:19:52 +0200

    Added a new print message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello");
 		System.out.println("we made a change");
+		System.out.println("we are on a new branch");
 	}	
 }
