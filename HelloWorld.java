e17e9ad7af9382599c1f37f0872fa4c02f33d230
abdelghanysd <arahman@singledigits.com>
2021-07-27 23:03:38 +0200

    Change format

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld{
-	public static void main(String[] args){
-		System.out.println("Hello");
-	}	
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
 }
